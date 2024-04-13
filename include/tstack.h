33e8cee8b591a885b91b29663da6be079bf799a2
polinamoskvina <160390879+polinamoskvina@users.noreply.github.com>
2024-04-13 11:57:32 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -3,8 +3,29 @@
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
-class TStack {
-  // добавьте код стека
+class TStack { 
+ private:
+    T arr[100];
+    int top;
+ public:
+    TStack(): top(-1) {}
+    void pushh(T val) {
+      if (!isOverflow())
+        arr[++top] = val;
+      else
+        throw std::string("Stack overflow");
+    }
+    T popp() {
+      if (isEmptyy())
+        throw std::string("Stack is empty");
+      else
+        return arr[top--];
+    }
+    bool isOverflow() const {
+      return top == size - 1;
+    }
+    bool isEmptyy() const {
+      return top == -1;
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
