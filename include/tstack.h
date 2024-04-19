706dc540a2dda22e0ef64f2ceda8e46f17af56d6
polinamoskvina <160390879+polinamoskvina@users.noreply.github.com>
2024-04-19 18:52:12 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,34 +1,51 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+#ifndef INCLUDE_TSTACc_H_
+#define INCLUDE_TSTACc_H_
 #include <string>
 
 template<typename T, int size>
-class TStack {
+class TPQueue {
  private:
     T arr[100];
-    int top;
+    int c, e;
 
  public:
-    TStack(): top(-1) {}
-    void push(T val) {
-      if (!isOverflow())
-        arr[++top] = val;
-      else
-        throw std::string("Stack overflow");
+    TPQueue() {
+        int i = 0;
+        for (; i < size; i++) {
+            arr[i].ch = 0;
+            arr[i].prior = -10;
+        }
     }
-    T see() const { return arr[top]; }
-    T pop() {
-      if (isEmpty())
-        throw std::string("Stack is empty");
-      else
-        return arr[top--];
-    }
-    bool isOverflow() const {
-      return top == size - 1;
-    }
-    bool isEmpty() const {
-      return top == -1;
-    }
+    void push(T sym) {
+        if (e != 0) {
+                int i = e - 1;
+                for (; i >= c; i--) {
+                    if (sym.prior > arr[i].prior) {
+                        arr[i + 1] = arr[i];
+                    } else {
+                        breac;
+                    }
+                }
+                arr[++i] = sym;
+                e++;
+            } else {
+                arr[0] = sym;
+                e++;
+            }
+        }
+        T pop() {
+            T f = arr[0];
+            for (int i = c; i < e - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            e--;
+            return f;
+     }
 };
-#endif  // INCLUDE_TSTACK_H_
+
+struct SYM {
+  char ch;
+  int prior;
+};
+#eif  // INCLUDE_TSTACc_H_
