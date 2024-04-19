2a0acaa995494e298f6a7a55e1a24eae7465edb7
polinamoskvina <160390879+polinamoskvina@users.noreply.github.com>
2024-04-19 19:07:03 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -3,82 +3,32 @@
 #define INCLUDE_TSTACc_H_
 #include <string>
 
-TStack<char, 100> stack1;
-TStack<int, 100> stack2;
+template<typename T, int size>
+class TStack {
+ private:
+    T arr[100];
+    int top;
 
-int prior(char operand) {
-    if (operand == '-') return 1;
-    if (operand == '+') return 1;
-    if (operand == '*') return 2;
-    if (operand == '/') return 2;
-    return 0;
-}
-
-std::string infx2pstfx(std::string inf) {
-    std::string post;
-    TStack<char, 100> stack;
-
-    for (char c : inf) {
-        if (isdigit(c)) {
-            post += c;
-            post += ' ';
-        } else if (c == '(') {
-            stack.push(c);
-        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-            while (!stack.isEmpty() && prior(stack.see()) >= prior(c)) {
-                post = post + stack.see() + ' ';
-                stack.pop();
-            }
-            stack.push(c);
-        } else if (c == ')') {
-            while (!stack.isEmpty() && stack.see() != '(') {
-                post = post + stack.see() + ' ';
-                stack.pop();
-            }
-            stack.pop();
-        }
+ public:
+    TStack(): top(-1) {}
+    void push(T val) {
+      if (!isOverflow())
+        arr[++top] = val;
+      else
+        throw std::string("Stack overflow");
     }
-    while (!stack.isEmpty()) {
-        post = post + stack.see() + ' ';
-        stack.pop();
+    T see() const { return arr[top]; }
+    T pop() {
+      if (isEmpty())
+        throw std::string("Stack is empty");
+      else
+        return arr[top--];
     }
-    if (!post.empty()) {
-        post.pop_back();
+    bool isOverflow() const {
+      return top == size - 1;
     }
-    return post;
-}
-
-int eval(std::string pref) {
-    std::string strNumber = "";
-    for (char c : pref) {
-        if (c != ' ' && c != '+' && c != '-' && c != '*' && c != '/') {
-            strNumber += c;
-        }
-        if (c == ' ') {
-            if (!strNumber.empty()) {
-                int num = std::stoi(strNumber);
-                stack2.push(num);
-                strNumber = "";
-            }
-        }
-        if (c == '+' || c == '-' || c == '*' || c == '/') {
-            int b = stack2.see();
-            stack2.pop();
-            int a = stack2.see();
-            stack2.pop();
-
-            if (c == '+') {
-                stack2.push((a + b));
-            } else if (c == '-') {
-                stack2.push((a - b));
-            } else if (c == '*') {
-                stack2.push((a * b));
-            } else if (c == '/') {
-                stack2.push((a / b));
-            }
-        }
+    bool isEmpty() const {
+      return top == -1;
     }
-
-    return stack2.see();
-}
-#endif
+};
+#endif  // INCLUDE_TSTACK_H_
